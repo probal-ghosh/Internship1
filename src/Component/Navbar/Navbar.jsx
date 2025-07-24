b269b36f64e3cbf9af450fa9327e07b041afe891
Probal Ghosh <probalghoshofficial@gmail.com>
2025-07-24 19:30:24 +0530

    navbar complete

--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -3,7 +3,23 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div>
-        
+        <nav className='flex justify-between items-center'>
+            <div className="text-2xl font-bold text-gray-800">
+          Kingsukh <span className="text-yellow-500">Guest House</span>
+        </div>
+            <div className='w-full flex justify-around'>
+                <ul className='flex justify-around ml-7 w-full'>
+                <li>Home</li>
+                <li>About</li>
+                <li>services</li>
+                <li>Rooms</li>
+                <li>Gallery</li>
+                <li>Contact</li>
+            </ul>
+            <span className='bg-indigo-700 text-center mr-5'>Book Now</span>
+            </div>
+
+        </nav>
     </div>
   )
 }
