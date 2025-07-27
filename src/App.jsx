37928deee8a88a651be0e71f577542316c20dbeb
Probal Ghosh <probalghoshofficial@gmail.com>
2025-07-27 11:43:49 +0530

    added iframe part

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import Services from './Component/Services/Services';
 import Stats from './Component/Stats/Stats';
 import Gallery from './Component/Gallery/Gallery';
 import Contact from './Component/Contact/Contact';
+import MapEmbed from './Component/Map/Map';
 
 function App() {
   useEffect(() => {
@@ -32,7 +33,7 @@ function App() {
       <Stats/>
       <Gallery/>
       <Contact/>
-      {/* <Map/> */}
+      <MapEmbed/>
     </div>
   )
 }
